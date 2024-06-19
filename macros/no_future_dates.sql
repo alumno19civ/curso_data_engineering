45393c0bf9028ca26f543885d95bd92c5e06a747
alumno19 civ <alumno19civ@gmail.com>
2024-06-19 17:57:03 +0000

    final

--- macros/no_future_dates.sql
+++ macros/no_future_dates.sql
@@ -1,8 +1,8 @@
 {% test no_future_dates(model, column_name) %}
    
-   select count(*)
+   select * --*
    from {{ model }}
    where {{column_name}} > CURRENT_TIMESTAMP
-   having count(*) > 1
+  -- having count(*) > 1
 
 {% endtest %}
